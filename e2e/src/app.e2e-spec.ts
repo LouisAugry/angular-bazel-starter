e91324025eba612ba4ef5bf66533483f95bd9fa2
LouisAugry <pro@louisaugry.fr>
2020-02-10 21:31:15 +0100

    Change css to scss, fix name of project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('test-bazel app is running!');
+    expect(page.getTitleText()).toEqual('angular-bazel-starter app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test-bazel'`, () => {
+  it(`should have as title 'angular-bazel-starter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-bazel');
+    expect(app.title).toEqual('angular-bazel-starter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-bazel app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-bazel-starter app is running!');
   });
 });
